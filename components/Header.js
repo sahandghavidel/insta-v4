72353d3d8563b66f836b879c6730f958261000f1
Sahandgha <sahand.gha@gmail.com>
2022-02-08 08:47:37 +1000

    get the session and modify the header component with google data

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,10 @@ import Image from "next/image";
 import React from "react";
 import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
 import { HomeIcon } from "@heroicons/react/solid";
-
+import { useSession, signIn, signOut } from "next-auth/react";
 export default function Header() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30">
       <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
@@ -39,12 +41,19 @@ export default function Header() {
 
         <div className="flex space-x-4 items-center">
           <HomeIcon className="hidden md:inline-flex  h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out" />
-          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out" />
-          <img
-            src="https://static.skillshare.com/uploads/users/350301760/user-image-large.jpg?753816048"
-            alt="user-image"
-            className="h-10 rounded-full cursor-pointer"
-          />
+          {session ? (
+            <>
+              <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out" />
+              <img
+                onClick={signOut}
+                src={session.user.image}
+                alt="user-image"
+                className="h-10 rounded-full cursor-pointer"
+              />
+            </>
+          ) : (
+            <button onClick={signIn}>Sign in</button>
+          )}
         </div>
       </div>
     </div>
