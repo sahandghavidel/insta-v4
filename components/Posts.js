f394dc64c8203e2e2a9d0d4a29d47ba9ad8c2f24
Sahandgha <sahand.gha@gmail.com>
2022-02-14 08:40:44 +1000

    get the post data from database and show it in the feed section

--- components/Posts.js
+++ components/Posts.js
@@ -1,35 +1,31 @@
-import React from 'react';
-import Post from './Post';
+import { useEffect, useState } from "react";
+import Post from "./Post";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function Posts() {
-    const posts =[
-        {
-            id: "1",
-            username: "codewithsahand",
-            userImg: "https://static.skillshare.com/uploads/users/350301760/user-image-large.jpg?753816048",
-            img: "https://images.unsplash.com/photo-1643818507403-a3ed10760d16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-            caption: "Nice picture"
-
-        },
-        {
-            id: "2",
-            username: "ghavidelsahand",
-            userImg: "https://static.skillshare.com/uploads/users/350301760/user-image-large.jpg?753816048",
-            img: "https://images.unsplash.com/photo-1643806720662-f9bc01be4e83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-            caption: "New picture from my city"
-
-        }
-    ]
-  return <div>
-        {posts.map(post =>(
-            <Post
-                key={post.id}
-                id={post.id}
-                username={post.username}
-                userImg={post.userImg}
-                img={post.img}
-                caption={post.caption}
-            />
-        ))}
-  </div>;
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "posts"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+    return unsubscribe;
+  });
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
+        />
+      ))}
+    </div>
+  );
 }
