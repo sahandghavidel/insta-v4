86f4dcd4c4a4d8045e044c1a0ae4cf3d44fc59b1
Sahandgha <sahand.gha@gmail.com>
2022-02-17 08:02:12 +1000

    show comments in the comments section and get data from the firebase database

--- components/Post.js
+++ components/Post.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Moment from 'react-moment';
 import { db } from "../firebase";
 import {
   DotsHorizontalIcon,
@@ -7,11 +8,18 @@ import {
   BookmarkIcon,
   EmojiHappyIcon,
 } from "@heroicons/react/outline";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 export default function Post({ img, userImg, caption, username, id }) {
   const { data: session } = useSession();
   const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "posts", id, "comments"), orderBy("timestamp", "desc")), (snapshot) => {setComments(snapshot.docs)}
+    )
+
+  }, [db, id])
   async function sendComment(event) {
     event.preventDefault();
     const commentToSend = comment;
@@ -59,6 +67,18 @@ export default function Post({ img, userImg, caption, username, id }) {
         <span className="font-bold mr-2">{username}</span>
         {caption}
       </p>
+      {comments.length > 0 && (
+        <div className="mx-10 max-h-24 overflow-y-scroll scrollbar-none">
+          {comments.map(comment =>(
+            <div className="flex items-center space-x-2 mb-2">
+              <img className="h-7  rounded-full object-cover"  src={comment.data().userImage} alt="user-image" />
+              <p className="font-semibold">{comment.data().username}</p>
+              <p className="flex-1 truncate">{comment.data().comment}</p>
+              <Moment fromNow>{comment.data().timestamp?.toDate()}</Moment>
+            </div>
+          ))}
+        </div>
+      )}
 
       {/* Post input box */}
       {session && (
