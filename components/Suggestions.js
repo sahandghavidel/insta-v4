be02260ad09196d2969924b9bba74a4f9baba25c
Sahandgha <sahand.gha@gmail.com>
2022-02-17 13:49:28 +1000

    add the key for suggestions.js

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -18,15 +18,28 @@ export default function Suggestions() {
         <h3 className="font-bold text-gray-400">Suggestion for you</h3>
         <button className="text-gray-600 font-semibold">See all</button>
       </div>
-      {suggestions.map(suggestion =>(
-          <div className="flex items-center justify-between mt-3">
-              <img className="h-10 rounded-full border p-[2px]"  src={`https://i.pravatar.cc/150?img=${Math.ceil(Math.random() * 70)}`} alt="" />
-              <div className="flex-1 ml-4">
-                  <h2 className="font-semibold text-sm">{suggestion.username}</h2>
-                  <h3 className="text-sm text-gray-400 truncate w-[230px]">{suggestion.jobTitle}</h3>
-              </div>
-              <button className="font-semibold text-blue-400 text-sm">Follow</button>
+      {suggestions.map((suggestion) => (
+        <div
+          key={suggestion.id}
+          className="flex items-center justify-between mt-3"
+        >
+          <img
+            className="h-10 rounded-full border p-[2px]"
+            src={`https://i.pravatar.cc/150?img=${Math.ceil(
+              Math.random() * 70
+            )}`}
+            alt=""
+          />
+          <div className="flex-1 ml-4">
+            <h2 className="font-semibold text-sm">{suggestion.username}</h2>
+            <h3 className="text-sm text-gray-400 truncate w-[230px]">
+              {suggestion.jobTitle}
+            </h3>
           </div>
+          <button className="font-semibold text-blue-400 text-sm">
+            Follow
+          </button>
+        </div>
       ))}
     </div>
   );
